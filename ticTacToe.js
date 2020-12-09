4866d4d234d76271d6d24c500687272881fd9488
Tristan Nelson <Tristan@tristans-mbp.lan>
2020-12-08 22:56:27 -0500

    Finally made some progress on getting the players to switch back and forth by tracking the clicks and using a mod if statement

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,47 +1,59 @@
+let allCells = document.querySelectorAll(".cell");
+console.log({ allCells });
+let playerX = "X";
+let playerO = "O";
+let currentPlayer = " ";
+let count = 0;
+let boardCount = document.getElementById("board");
+boardCount.onclick = function () {
+  count++;
+  document.getElementById("statusBox").innerHTML = "Turn: " + count;
+};
+
 function startGame() {
   document.getElementById("start").disabled = true;
 
-  //increment the counter adding 1 each iteration
-  let counter = 0;
-  while (counter < 9) {
-    for (let cell of allCells) {
-      let playerX = "X";
-      let playerO = "O";
-      let currentPlayer = " ";
-      if (counter % 2 === 0) {
-        currentPlayer = playerX;
-      } else {
-        currentPlayer = playerO;
-      }
-      cell.addEventListener("click", function (theEvent) {
-        console.log({ theEvent });
-        counter++;
-        return (theEvent.target.textContent = currentPlayer);
-      });
-    }
+  if (count % 2 === 0) {
+    currentPlayer = playerX;
+  } else {
+    currentPlayer = playerO;
   }
-}
 
-function canMoveTo() {
-  if (allCells === "X" || allCells === "O") {
-    return false;
+  if (
+    (document.getElementsByClassName(".cellone").innerHTML &&
+      document.getElementsByClassName(".celltwo").innerHTML &&
+      document.getElementsByClassName(".cellthree").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x") ||
+    (document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML &&
+      document.getElementsByClassName(".cell").innerHTML === "x")
+  ) {
+    alert("Winner");
+    let currentPlayer = document.getElementById("playerStatus").innerHTML;
+    return currentPlayer;
   }
-  return true;
-}
-let allCells = document.querySelectorAll(".cell");
-console.log({ allCells });
 
-function makeMove(mark, cell, allCells) {
-  if (canMoveTo(cell)) {
-    //make the move, then return a status and board with changes applied
-    allCells = mark;
-    return {
-      status: "success",
-      newBoard: allCells,
-    };
+  for (let cell of allCells) {
+    cell.addEventListener("click", function (theEvent) {
+      console.log({ theEvent });
+      return (theEvent.target.textContent = currentPlayer);
+    });
   }
-  return {
-    status: "failure",
-    newBoard: allCells,
-  };
 }
